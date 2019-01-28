f0383b9301e27eebd09ad4693893a2291e11b51c
amkessler <kessler.aaron@gmail.com>
2019-01-28 18:40:41 -0500

    first crack at faceted bar charts for candidates by state tab

--- 00_gsheets_setup.R
+++ 00_gsheets_setup.R
@@ -4,17 +4,19 @@ library(janitor)
 library(googlesheets)
 library(gt)
 library(ggmap)
+library(plotly)
+library(RColorBrewer)
 
 #this will trigger a web page to authenticate with google account
-gs_ls() %>% View()
+# gs_ls() %>% View()
 
 
 #register DW's 2020 google sheet
 dw2020 <- gs_key("1rhCkWIHW3kblAP2H-1A7_123ACCuJLoafGXfnEldYl4")
 
 #open in brower
-dw2020 %>% 
-  gs_browse()
+# dw2020 %>% 
+#   gs_browse()
 
 #list worksheets in the google sheet
 gs_ws_ls(dw2020)
@@ -27,6 +29,22 @@ gs_ws_ls(dw2020)
 events <- dw2020 %>% 
   gs_read(ws = "AK VER Rally / Event Tracker") %>% 
   clean_names() 
+
+#set formatting of certain columns
+events <- events %>% 
+  mutate(
+    date = mdy(date),
+    thru_date = mdy(thru_date),
+    cand_fullname = as.factor(cand_fullname),
+    state = as.factor(state),
+    cd_if_known = as.factor(cd_if_known),
+    event_type = as.factor(event_type),
+    cand_fullname = as.factor(cand_fullname),
+    cand_lastname = as.factor(cand_lastname),
+    sponsor = as.factor(sponsor)
+  )
+
+
 
 
 #some exploring 
@@ -154,3 +172,59 @@ keyremarks %>%
 keyremarks %>%
   count(venue) %>%
   arrange(desc(n))
+
+
+
+
+#### FACET CHARTS FOR CANDIDATES BY STATE ####
+
+
+#by state counts
+by_cand_and_state <- events %>% 
+  filter(date < Sys.Date(),
+         state != "INTL") %>% 
+  count(cand_lastname, state) %>% 
+  arrange(cand_lastname, desc(n)) 
+
+#reorder factors to order bars descending
+bystate <- bystate %>% 
+  mutate(
+    state = as.factor(state),
+    state = fct_reorder(state, desc(n))
+  )
+
+
+
+colourCount = length(unique(by_cand_and_state$state))
+getPalette = colorRampPalette(brewer.pal(9, "Set2"))
+
+
+d <- ggplot(data = by_cand_and_state, aes(x = state, y = n, fill = state)) +
+  geom_col() +
+  # coord_flip() + 
+  theme_minimal() +
+  scale_fill_manual(values = getPalette(colourCount))
+  # scale_fill_brewer(palette="Set3")
+
+d
+
+d2 <- d + labs(x ="", y = "") +
+  theme(legend.title=element_blank()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+
+d3 <- d2 + facet_wrap(~cand_lastname)
+
+d3
+
+dd <- ggplotly(d3) 
+
+dd
+
+dd_nomenu <- dd %>% config(displayModeBar = FALSE)
+dd_nomenu
+
+
+
+# https://www.r-bloggers.com/how-to-expand-color-palette-with-ggplot-and-rcolorbrewer/
+  
