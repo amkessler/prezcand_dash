1a0781ccfd6bb7b7f5324227462a04746a24f327
Aaron Kessler <kessler.aaron@gmail.com>
2019-01-31 16:56:30 -0500

    create R file for building daily graphic exports for digital stories

---
+++ 02_fordigitalstories.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(glue)
+library(plotly)
+library(DT)
+library(googlesheets)
+library(kableExtra)
+library(leaflet)
+# library(ggmap)
+library(RColorBrewer)
+library(htmlwidgets)
+library(htmltools)
+library(tigris)
+options(tigris_class = "sf")
+
+
+
+# to pull from live data
+
+
+
+
+# to pull from saved temp file offline
